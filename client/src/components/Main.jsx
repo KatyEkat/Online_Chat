b340c5429a890c250fef0212b1adbf57c313ff63
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-05-10 13:05:50 +0300

    Logical for main page, added inputs and join button

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../styles/Main.module.css";
+import { Link } from "react-router-dom";
+
+const FIELDS = {
+  USERNAME: "username",
+  ROOM: "room",
+};
 
 const Main = () => {
-  return <div>Main</div>;
+  const { USERNAME, ROOM } = FIELDS;
+
+  const [value, setValue] = useState({ [USERNAME]: "", [ROOM]: "" });
+
+  const handleChange = ({ target: { value, name } }) => {
+    setValue({ ...value, [name]: value });
+  };
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Join</h1>
+
+        <form className={styles.form}></form>
+        <div className={styles.group}>
+          <input
+            type="text"
+            name="username"
+            placeholder="User Name"
+            value={value[USERNAME]}
+            className={styles.input}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+        </div>
+        <div className={styles.group}>
+          <input
+            type="text"
+            name="room"
+            placeholder="Room name"
+            value={value[ROOM]}
+            className={styles.input}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        <Link to={`/chat?name=${value[USERNAME]}&room=${value[ROOM]}`}>
+          <button type="submit" className={styles.button}>
+            Sign in
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
